4edda234bd434acdfe1e784885a7a6e20d5aa9b4
Misaki Agata <p17004am@gm.tsuda.ac.jp>
2020-11-07 22:05:22 +0900

    add FlatList

--- screens/RecommendScreen.js
+++ screens/RecommendScreen.js
@@ -1,7 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image, Animated, PanResponder, TouchableOpacity, FlatList, SafeAreaView } from 'react-native';
 
 export default class RecommendScreen extends React.Component {
+
+  state = {
+    dummyData: [
+      {
+        id: '0',
+        image: './assets/restaurant/1.jpg',
+        title: 'First Item',
+      },
+      {
+        id: '1',
+        image: './assets/restaurant/2.jpg',
+        title: 'Second Item',
+      },
+      {
+        id: '2',
+        image: './assets/restaurant/3.jpg',
+        title: 'Third Item',
+      },
+    ]
+  } 
 
   constructor() {
     super()
@@ -10,16 +30,32 @@ export default class RecommendScreen extends React.Component {
   componentWillMount() {
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>RecommendScreen</Text>
+  renderItem = ({ item }) => {
+    <TouchableOpacity style={styles.listItem}>
+      <View>
+        <Image source={{uri: item.image}} />
       </View>
+    </TouchableOpacity>
+  }
 
+  render() {
+    const { dummyData } = this.state
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          style={styles.flatList}
+          data={dummyData}
+          keyExtractor={[item => item.id]}
+          numColumns={2}
+          renderItem={this.renderItem}
+        />
+      </SafeAreaView>
     );
   }
 }
 
+const screenHeight = Dimensions.get('window').height
+const screenWidth = Dimensions.get('window').width
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -27,4 +63,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  flatList: {
+    height: screenHeight,
+    width: screenWidth,
+    backgroundColor: 'red'
+  },
+  listItem: {
+    width: screenWidth,
+    height: 48,
+    backgroundColor: 'white'
+  }
 });
