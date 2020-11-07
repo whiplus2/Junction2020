64445f18069d3c496c0de2794fb3a7be33083f87
Misaki Agata <p17004am@gm.tsuda.ac.jp>
2020-11-07 20:14:29 +0900

    fix home and swipe screen ui

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,50 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, Dimensions, View, TouchableOpacity} from 'react-native';
-
-const SCREEN_WIDTH = Dimensions.get('window').width
-
+import { StyleSheet, Text, Dimensions, View, TouchableOpacity, Image} from 'react-native';
 export default class HomeScreen extends React.Component {
-
   constructor() {
     super()
   }
-
   componentWillMount() {
   }
-
   showSwipeScreen = (type) => {
     const { navigation } = this.props
     navigation.navigate('Swipe', {Swipe: {type: type}})
   }
-
   render() {
     return (
       <View style={styles.container}>
-
-        <View style={{ height: 60 }}></View>
-
+        <View>
+          <Text style={styles.headerText}>Hey! What do you feel{"\n"}like doing today?</Text>
+        </View>
         <TouchableOpacity onPress={() => this.showSwipeScreen('eat')}>
-            <View style={{height: 80, width: SCREEN_WIDTH, backgroundColor: 'yellow'}}>
-                <Text>Eat</Text>
-            </View>
+          <Image style={styles.button} source={require('../assets/group1.png')}>
+          </Image>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => this.showSwipeScreen('drink')}>
-            <View style={{height: 80, width: SCREEN_WIDTH, backgroundColor: 'red'}}>
-                <Text>Drink</Text>
-            </View>
+          <Image style={styles.button} source={require('../assets/group2.png')}>
+          </Image>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => this.showSwipeScreen('work')}>
-            <View style={{height: 80, width: SCREEN_WIDTH, backgroundColor: 'blue'}}>
-                <Text>Work</Text>
-            </View>
+          <Image style={styles.button} source={require('../assets/group3.png')}>
+          </Image>
         </TouchableOpacity>
-
       </View>
-
     );
   }
 }
-
+const screenWidth = Dimensions.get('window').width
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -52,4 +40,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerText: {
+    fontSize: 24,
+    fontWeight: '500',
+    textAlign: 'center',
+    marginBottom: 28,
+  },
+  button: {
+    height: 160,
+    width: screenWidth*0.9,
+    marginVertical: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: '500',
+  }
 });
